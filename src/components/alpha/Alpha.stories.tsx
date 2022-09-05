34d3e06faf389cf86e0aa18bc1fda69053a0ae43
Reece Carolan <reece@mycopilot.com>
2022-09-04 22:25:29 -0700

    docs: add slider and alpha stories

---
+++ src/components/alpha/Alpha.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AlphaComponent from "./Alpha";
+
+export default {
+  title: "Pickers/Alpha",
+  component: AlphaComponent,
+};
+
+export const Alpha = () => <AlphaComponent />;
---
+++ src/components/slider/Slider.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SliderComponent from "./Slider";
+
+export default {
+  title: "Pickers/Slider",
+  component: SliderComponent,
+};
+
+export const Slider = () => <SliderComponent />;
